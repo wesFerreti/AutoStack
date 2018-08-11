f5733f1ed27de47ba978b5efeb0f7ba30f08bf63
Wesley Ferreti <w3sl3y.ferret1@gmail.com>
2018-08-11 00:51:59 -0300

    playing with graphs and angular material grids

--- src/app/global-active-power-chart/global-active-power-chart.component.ts
+++ src/app/global-active-power-chart/global-active-power-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js';
+
 
 @Component({
   selector: 'app-global-active-power-chart',
@@ -8,14 +8,33 @@ import Chart from 'chart.js';
 })
 export class GlobalActivePowerChartComponent implements OnInit {
 
+  WindPlantActivePower: number = 300;
+  HidroPlantActivePower: number = 100;
+  SolarPlantActivePower:number = 400;
+  breakpoint;
+  rowHeights;
   constructor() { }
 
   ngOnInit() {
+      console.log(window.innerWidth);
+      this.breakpoint = (window.innerWidth <= 400) ? 1 : 6;
+      this.rowHeights = (window.innerWidth <= 400) ? 500 : 800;
+      console.log(this.rowHeights);
   }
-  public doughnutChartLabels:string[] = ['WindPower', 'HidroPower', 'SolarPower'];
-  public doughnutChartData:number[] = [350, 450, 100];
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 6;
+    this.rowHeights = (event.target.innerWidth <= 400) ? 500: 800;
+  }
+  public doughnutChartLabels:string[] = ['WindPower', 'HidroPower', 'SolarPower', 'NotGenerating'];
+  public doughnutChartData:number[] = [this.WindPlantActivePower, this.HidroPlantActivePower,this.SolarPlantActivePower,222];
   public doughnutChartType:string = 'doughnut';
- 
+  
+  public chartColors: any[] = [
+    { 
+      backgroundColor:["#b99dea", "#9e79e0", "#7a49d1"] 
+      
+    }];
   // events
   public chartClicked(e:any):void {
     console.log(e);
@@ -24,6 +43,7 @@ export class GlobalActivePowerChartComponent implements OnInit {
   public chartHovered(e:any):void {
     console.log(e);
   }
+
 
 }
 
